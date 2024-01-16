b7845805586de9d6a2c506ba9fea2863491a7592
Harsh Jain <jainharsh1126@gmail.com>
2024-01-16 13:41:03 +0530

    initialize spring boot project

---
+++ src/test/java/com/anytimedev/StudentCURD/StudentCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anytimedev.StudentCURD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
